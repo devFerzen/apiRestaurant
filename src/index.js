c84a50c0c271c2cfc7554171fb75df62a88adbc8
devFerzen <alanferzen.ss@gmail.com>
2020-03-08 15:17:44 -0600

    -- Controlador post para añadir nuevos restaurantes

--- src/index.js
+++ src/index.js
@@ -10,6 +10,10 @@ import routes from './routes';
 let app = express();
 app.server = http.createServer(app);
 
+//Configure bodyParser to add data from the body
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+
 //middleware
 
 //passportConfig
@@ -17,7 +21,7 @@ app.server = http.createServer(app);
 //Mandar todas las url /v1 a la carpeta routes
 app.use('/v1',routes);
 app.server.listen(config.port);
-console.log('Start on port ${app.server.address().port}');
+console.log('Start on port ' + app.server.address().port);
 
 //exporting this app as default
 export default app;
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import middleware from '../middleware';
 import inicializedDB from '../db.js'; //Inicializará la conexion a la bd
 import config from '../config';
+import restaurant from '../controller/restaurant';
 
 let router = express.Router();
 
@@ -9,7 +10,13 @@ inicializedDB(db=>{
   //Middleware to connect to db
   router.use(middleware({config,db}));
 
+  //Test connection
+  router.get('/',(req,res)=>{
+    res.json({"message":"Hi"});
+  });
+
   //api routes /v1
+  router.use('/restaurant',restaurant({config,db}));
 
 });
 
