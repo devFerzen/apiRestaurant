ebccaef42f229c9971bf1c4fdb7b75bdc1d57e82
devFerzen <52632495+devFerzen@users.noreply.github.com>
2020-09-09 20:50:14 -0500

    Cambio en DB

--- src/db.js
+++ src/db.js
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 import config from './config';
-
+//DB CHANGE
 //Cuando se importe esto conectará a nuestra BD
 export default callback => {
   let db = mongoose.connect(config.mongoUrl);
