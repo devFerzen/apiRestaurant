e9c7cbfcbe4d24e5db36b10f5c444843541fb0c6
devFerzen <52632495+devFerzen@users.noreply.github.com>
2020-09-09 20:51:25 -0500

    Cambios en branch 2

--- src/db.js
+++ src/db.js
@@ -1,8 +1,11 @@
 import mongoose from 'mongoose';
 import config from './config';
 
+
+//Otros cambios aquí
 //Cuando se importe esto conectará a nuestra BD
 export default callback => {
   let db = mongoose.connect(config.mongoUrl);
   callback(db);
 }
+//Otros cambios acá
